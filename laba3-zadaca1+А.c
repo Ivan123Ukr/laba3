09eaa4c949883a676c6e970654cb5cca9553564a
Ivan123Ukr <slaninaivan41@gmail.com>
2025-05-13 18:27:49 +0300

    Update laba3-zadaca1+А.c

--- laba3-zadaca1+А.c
+++ laba3-zadaca1+А.c
@@ -1,11 +1,34 @@
-# Задані цілі числа s і t
-s = ...  # Введіть значення
-t = ...  # Введіть значення
+#include <stdio.h>
+#include <math.h>
 
-k = (t**2 + 2 * s**2 * t + s) if s < 4 else ((s + 5) / (-t))**0.5 if s == 4 else 50 + s**2 + t
+int main() {
+    int s, t;
+    double k;
 
-# Перевіримо, чи результат можна обчислити
-try:
-    print("k =", k)
-except ValueError:
-    print("Неможливо обчислити значення виразу")
+    // Введення значень s і t
+    printf("Введіть ціле число s: ");
+    scanf("%d", &s);
+    printf("Введіть ціле число t: ");
+    scanf("%d", &t);
+
+    // Обчислення значення k згідно з умовами
+    if (s < 4) {
+        k = pow(t, 2) + 2 * pow(s, 2) * t + s;
+        printf("k = %.2f\n", k);
+    }
+    else if (s == 4) {
+        double value = (s + 5.0) / (-t);
+        if (value < 0) {
+            printf("Неможливо обчислити значення виразу (підкореневий вираз < 0)\n");
+        } else {
+            k = sqrt(value);
+            printf("k = %.2f\n", k);
+        }
+    }
+    else { // s > 4
+        k = 50 + pow(s, 2) + t;
+        printf("k = %.2f\n", k);
+    }
+
+    return 0;
+}
